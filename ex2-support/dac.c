9076e75984e49aee21effac3f40a156e6471e911
prestegaard <hakonp@gmail.com>
2016-10-04 12:18:38 +0200

    Halfway done with DAC

--- ex2-support/dac.c
+++ ex2-support/dac.c
@@ -10,7 +10,14 @@ void setupDAC()
 
 	   1. Enable the DAC clock by setting bit 17 in CMU_HFPERCLKEN0
 	   2. Prescale DAC clock by writing 0x50010 to DAC0_CTRL
-	   3. Enable left and right audio channels by writing 1 to DAC0_CH0CTRL and DAC0_CH1CTRL
-	   4. Write a continuous stream of samples to the DAC data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a timer interrupt
+	   3. Enable left and right audio channels by writing 1 to DAC0_CH0CTRL
+	      and DAC0_CH1CTRL
+	   4. Write a continuous stream of samples to the DAC data registers, 
+	   	  DAC0_CH0DATA and DAC0_CH1DATA, for example from a timer interrupt
 	 */
+	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_DAC0;	/* enable DAC clock */
+	*DAC0_CTRL = 0x50010; /* Set prescalar, CHECK OUT WHICH PRESCALER THIS IS */
+	*DAC0_CH0CTRL = 1; /* enable left  audio channel */
+	*DAC0_CH1CTRL = 1; /* enable right audio channel */
+
 }
