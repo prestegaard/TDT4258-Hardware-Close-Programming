5f400f44644bd5687b0c05efad33bdac7be4277b
prestegaard <hakon_p@hotmail.com>
2016-11-11 11:12:35 +0100

    More ideas implemented on the snake game

--- ex3/local_src/game-1.0/temp_snake_functionality.c
+++ ex3/local_src/game-1.0/temp_snake_functionality.c
@@ -32,7 +32,7 @@ void fill_rectangle(uint16_t x_pos_left, uint16_t x_pos_right, uint16_t y_pos_to
 
 
 
-/* display 
+ 
 
 
 0: 0 ---------------- 319
@@ -60,11 +60,19 @@ void draw_snake(...){
 enum uint8_t direction = {up, down, left, rigth};
 
 struct snake{
+	Color color_background;
+	Color color_body;
+	Color color_food;
+	uint8_t food_x;
+	uint8_t food_y;
 	uint8_t direction;
 	uint8_t length;
-	int8_t head_x;
-	int8_t head_y;
-	uint8_t [2]* snake_trace;
+	int8_t head_x; //position can be -1, then you have lost
+	int8_t head_y; //position can be -1, then you have lost
+	uint8_t tail_x;
+	uint8_t tail_y;
+	uint8_t [40][2] snake_trace; //trace[n][0] is x, trace[n][1] is y
+
 }
 
 game (..)
@@ -78,7 +86,8 @@ play_snake(..){
 	uint16_t* snake;
 
 	while(not_looser){
-		draw_snake(...);
+		
+		calculate_next_round();
 		update_display();
 		u_sleep(difficulty);
 	}
@@ -94,32 +103,78 @@ altså nede på 5 og høyre på 5 er blank
 
 spillbrett på 200x200 pixler, det gir et brett på 40x40 slangedeler
 
-uint8_t spillbrett[40][40];
-
-	uint8_t x,y;
+	uint8_t i,x,y;
 	switch snake.direction{
-	case up:
-		snake.head_y=snake.head_y+1;'
-		if(snake.head_y>=40){
-			you_loose==1;
-		}
-	case down:
-		snake.head_y=snake.head_y-1;'
-		if(snake.head_y<=0){
-			you_loose==1;
-		} 
-	
-
-	snake.trace[head_x][head_y]=1;
-	for(x=0;x<40;x++){
-		for(y=0;y<40;y++){
-			if(snake.trace[x][y]){
-
-				update_display();
+		case up:
+			snake.head_y += 1;
+			if(snake.head_y > 39){
+				you_loose=1;
 			}
-		}
+		case down:
+			snake.head_y -= 1;
+			if(snake.head_y < 0){
+				you_loose=1;
+			} 
+		case left:
+			snake.head_x -= 1;
+			if(snake.head_x < 0){
+				you_loose=1;
+			}
+		case rigth:
+			snake.head_x += 1;
+			if(snake.head_x > 39){
+				you_loose=1;
+			}
+	if(you_loose == 1){
+		return snake.length;
+	}
+
+	tail_x = snake.trace[0][0];
+	tail_y = snake.trace[0][y];
+	//update trace, remove tail, add head.
+	//todo: something other when food is eaten
+	for(i=0; i<snake.length -1; i++){
+		snake.trace[i][0] = snake.trace[i+1][0];
+		snake.trace[i][1] = snake.trace[i+1][1];
+	}
+	snake.trace[snake.length-1][head_x]=1;
+	snake.trace[snake.length-1][head_y]=1;
+
+
+	//this could be done before trace is updated ..
+	if(snake.head_x == food_x && snake.head_y == food_y){
+		snake.length += 2;
+	}
+
+	update_snake_on_diplay(&snake){
+		uint16_t head_x, head_y, tail_x, tail_y;
+		Rectangle head, tail, food;
+		//Convert snake tail and head game positions into display positions
+		head.x = 60  + (5*snake.head_x);
+		head.y = 220 - (5*snake.head_y);
+		tail.x = 60  + (5*snake.tail_x);
+		tail.y = 220 - (5*snake.tail_y);
+		food.x = 61  + (5*snake.food_x);
+		food.y = 220 - (5*snake.food_y);
+		
+		head.width  = 4;
+		head.height = 4;
+		tail.width  = 4;
+		tail.height = 4;
+
+		food.width  = 2;
+		food.height = 2;
+
+		//add new head
+		//remove tail from display
+		fill_rectangle(food, snake.food_food);
+		fill_rectangle(tail, snake.color_background);
+		fill_rectangle(head, snake.color_body);
+		update_display(food);
+		update_display(tail);
+		update_display(head);
 	}
 
 
 
-*/
+
